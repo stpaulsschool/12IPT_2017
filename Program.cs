bdf0a4e041b2c5e1cb4f84ec8e949bcb3b7e12b8
dfenwicksps <d.fenwick@stpauls.qld.edu.au>
2017-02-14 08:44:33 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            if (x == 5 && y == 7)
+            {
+                Console.WriteLine("This is true.");
+            }
+
+            if (x == 5 || y == 9)
+            {
+                Console.WriteLine("This is also true.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
