ddbd99b877ea45eb51927bfcd2a2cfccae162f2f
ac1999 <andrewcrouch@westlat.com.au>
2017-02-14 08:50:09 +1000

    Add files via upload

---
+++ Andrew_C/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1, var2 = 5, var3 = 6;
+            var1 = var2++ * --var3;
+            Console.WriteLine(var1);
+            Console.ReadLine();
+        }
+    }
+}
