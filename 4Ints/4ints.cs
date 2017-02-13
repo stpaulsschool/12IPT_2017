07159475151d43811e46a5d296a33b443f06e068
Sacred007 <Sacred007@users.noreply.github.com>
2017-02-14 09:50:28 +1000

    Create 4ints.cs

---
+++ 4Ints/4ints.cs
@@ -0,0 +1,23 @@
+/*Four Number Product Generator*/
+	Console.WriteLine("Hello!");
+	Console.Write("What is your name? ");
+	string userName = Convert.ToString(Console.ReadLine());
+	Console.WriteLine($"Hello {userName}, nice to meet you!");
+	Console.WriteLine("This program is designed to find the product of 4 integers the user, you, enters.");
+	string programRepeat = "Y";
+	while (programRepeat == "Y")
+	{
+		Console.WriteLine("Enter your 4 integers:");
+		Console.Write("     Integer One: ");
+		int valOne = Convert.ToInt32(Console.ReadLine());
+		Console.Write("     Integer Two: ");
+		int valTwo = Convert.ToInt32(Console.ReadLine());
+		Console.Write("     Integer Three: ");
+		int valThree = Convert.ToInt32(Console.ReadLine());
+		Console.Write("     Integer Four: ");
+		int valFour = Convert.ToInt32(Console.ReadLine());
+		int valProduct = valOne * valTwo * valThree * valFour;
+		Console.WriteLine($"The product of the integers you entered is: {valProduct}");
+		Console.WriteLine($"Would you like to continue {userName}? Enter Y if yes.");
+		programRepeat = Console.ReadLine();
+	}
