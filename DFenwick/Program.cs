e97f7b178e8fbae7ded292c8ad94228ca2248043
dfenwicksps <d.fenwick@stpauls.qld.edu.au>
2017-02-14 08:51:59 +1000

    RandomAndUnique.cs
    RandomAndUnique example

---
+++ DFenwick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomAndUnique
+{
+    class Program
+    {
+        static Random obj = new Random(); //seeding CPU clock
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPseudoRandom());
+            Console.WriteLine(GetPseudoRandom());
+            Console.WriteLine(GetPseudoRandom());
+            Console.WriteLine(GetPseudoRandom());
+            Console.ReadKey();
+        }
+        private static string GetPseudoRandom()
+        {
+            //Random obj = new Random();
+            return obj.Next(1, 50).ToString();
+        }
+    }
+}
