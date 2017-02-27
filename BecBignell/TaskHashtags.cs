428ba587759a43069a1a6ab6c346e5ea46084eca
bec121200 <15355@stpauls.qld.edu.au>
2017-02-27 19:47:45 +1000

    Add files via upload

---
+++ BecBignell/TaskHashtags.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int y = 1; y <= 5; y++)
+            {
+                for (int x = 1; x <= y; x++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
